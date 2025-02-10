18c26e92d13757684bc7a8374c9301ec088378ac
craft war <bay.asanov@mail.ru>
2025-02-10 14:12:51 +0600

    feat: add new properties and update project configuration
    Added project reference to `NedaoObjects.csproj` in `Necromancers- Doctors- and Other.csproj`.
    Changed `MaxHealth` and `Gain` types from `int` to `float`.
    Added new properties: `Damage`, `Armor`, `Speed`, `AttackSpeed`, `AttackRange`, and `BaseAttackTime`.

--- Godot/Scripts/GainProxy.cs
+++ Godot/Scripts/GainProxy.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +8,45 @@ using System.Threading.Tasks;
 [GlobalClass]
 public partial class GainProxy: Resource
 {
-    [Export]
-    public int MaxHealth
-    {
-        get; set;
-    }
+	[Export]
+	public float MaxHealth
+	{
+		get; set;
+	}
+
+	[Export]
+	public float Damage
+	{
+		get; set;
+	}
+
+	[Export]
+	public float Armor
+	{
+		get; set;
+	}
+
+	[Export]
+	public float Speed
+	{
+		get; set;
+	}
+
+	[Export]
+	public float AttackSpeed
+	{
+		get; set;
+	}
+
+	[Export]
+	public float AttackRange
+	{
+		get; set;
+	}
+
+	[Export]
+	public float BaseAttackTime
+	{
+		get; set;
+	}
 }
--- Godot/Scripts/NedaoProxy.cs
+++ Godot/Scripts/NedaoProxy.cs
@@ -1,22 +1,161 @@
 using Godot;
+using Godot.Collections;
+using NedaoObjects;
+using NedaoObjects.Gains;
 using System;
 
 public partial class NedaoProxy : Node
 {
+    /// <summary>
+    /// Prefers to use the target property instead of this field.
+    /// </summary>
+    private NedaoObject? _target;
+
+    [ExportGroup("Stats")]
+	[Export]
+	public float MaxHealth
+	{
+		get; set;
+	}
+
+    [Export]
+    public float Health
+    {
+		get; set;
+    }
+
+    [Export]
+	public float Damage
+	{
+		get; set;
+	}
 
 	[Export]
-    public GainProxy Gain
+	public float Armor
+	{
+		get; set;
+	}
+
+	[Export]
+	public float Speed
+	{
+		get; set;
+	}
+
+	[Export]
+	public float AttackSpeed
+	{
+		get; set;
+	}
+
+	[Export]
+	public float AttackRange
+	{
+		get; set;
+	}
+
+	[Export]
+	public float BaseAttackTime
+	{
+		get; set;
+	}
+
+	[ExportGroup("Level")]
+	[Export]
+    public int Level
+	{
+		get; set;
+	}
+
+	[Export]
+	public int Exp
+	{
+		get; set;
+	}
+
+    /// <summary>
+    /// If true, the level is set before gaining experience.
+    /// Be aware that the level may increase as a result.
+    /// </summary>
+    [Export]
+    /// <summary>
+    /// If true, the level is set before gaining experience.
+    /// Be aware that the level may increase as a result.
+    /// </summary>
+    public bool SetLevelBeforeGain
+	{
+		get; set;
+	}
+
+	[Export]
+	public GainProxy Gain
 	{
 		get; set;
 	} = new();
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public NedaoObject Target
 	{
+		get => _target ??= CreateTarget();
+    }
+
+
+    public override void _Ready()
+    {
+        Target.Helth = Health;
+        Target.MaxHealth.BaseValue = MaxHealth;
+        Target.Damage.BaseValue = Damage;
+        Target.Armor.BaseValue = Armor;
+        Target.Speed.BaseValue = Speed;
+        Target.AttackSpeed.BaseValue = AttackSpeed;
+        Target.AttackRange.BaseValue = AttackRange;
+        Target.BaseAttackTime.BaseValue = BaseAttackTime;
+
+        if (SetLevelBeforeGain)
+        {
+            Target.Level = Level;
+
+            // Warning: The level may increase, which may not be intended.
+            Target.TakeExp(Exp);
+        }
+
+        if (Target.GainModifier is SimpleGainModifier gainModifier)
+        {
+            gainModifier.MaxHealth.BaseValue = MaxHealth;
+            gainModifier.Damage.BaseValue = Damage;
+            gainModifier.Armor.BaseValue = Armor;
+            gainModifier.Speed.BaseValue = Speed;
+            gainModifier.AttackSpeed.BaseValue = AttackSpeed;
+            gainModifier.AttackRange.BaseValue = AttackRange;
+            gainModifier.BaseAttackTime.BaseValue = BaseAttackTime;
+        }
+
+        if (!SetLevelBeforeGain)
+        {
+            Target.Level = Level;
+
+            // Warning: The level may increase, which may not be intended.
+            Target.TakeExp(Exp);
+        }
+    }
+
+    /// <summary>
+    /// Creates and initializes a new <see cref="NedaoObject"/> instance.
+    /// This method is called lazily when accessing the <see cref="Target"/> property for the first time.
+    /// Designed to be overridden in derived classes to customize object creation.
+    /// </summary>
+    /// <returns>A new instance of <see cref="NedaoObject"/>.</returns>
+    protected virtual NedaoObject CreateTarget()
+	{
+		return new();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private static int ValidateLevel(int value)
 	{
+        if (value < 1)
+		{
+			return 1;
+		}
+
+		return value > NedaoObject.MaxLevel ? NedaoObject.MaxLevel : value;
 	}
 }
