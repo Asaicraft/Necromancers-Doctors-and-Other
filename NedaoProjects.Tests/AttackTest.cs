a078db45d6f3abd76a8a2c0374af7a92608e26fa
craft war <bay.asanov@mail.ru>
2025-02-11 22:43:11 +0600

    test: add unit test for TryAttackNedao
    Added `TestAttack` method in `AttackTest` to verify attack behavior.
    Checks if an attack reduces target health and respects cooldown mechanics.

---
+++ NedaoProjects.Tests/AttackTest.cs
@@ -0,0 +1,35 @@
+﻿using NedaoObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NedaoProjects.Tests;
+
+public class AttackTest
+{
+    [Fact]
+    public void TestAttack()
+    {
+        var attacker = new NedaoObject();
+        attacker.MaxHealth.BaseValue = 100;
+        attacker.Helth = 100;
+        attacker.Damage.BaseValue = 10;
+
+        attacker.AttackBehavior.Cooldown = -0;
+
+        var target = new NedaoObject();
+        target.MaxHealth.BaseValue = 100;
+        target.Helth = 100;
+
+        var isAttackSuccessful = attacker.TryAttackNedao(target);
+
+        Assert.True(isAttackSuccessful);
+        Assert.Equal(90, target.Helth);
+
+        isAttackSuccessful = attacker.TryAttackNedao(target);
+
+        Assert.False(isAttackSuccessful);
+    }
+}
