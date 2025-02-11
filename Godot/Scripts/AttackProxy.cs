b69911f5858f1a306849b24e84d5c454f10ddb56
craft war <bay.asanov@mail.ru>
2025-02-11 23:37:57 +0600

    refactor: update scenes and add new components
    Replaced `CollisionShape2D` and `Area2D` with `PackedScene` references for `MobCollision` and `AttackProxy` in `Gravedigger.tscn` and `NedaoProxy.tscn`.
    Updated `Tombstone.tscn` to use `Script` reference instead of `PackedScene`, added `Resource` references, and changed `Tombstone` node to `CharacterBody2D`.
    Created `AttackProxy.tscn` and `MobCollision.tscn` with proper node configurations.
    Reformatted `AttackProxy.cs` and updated `using` directives.
    Added `Enemy.cs`, defining `Enemy` class inheriting from `NedaoProxy`.

--- Godot/Scripts/AttackProxy.cs
+++ Godot/Scripts/AttackProxy.cs
@@ -1,4 +1,4 @@
-﻿using Godot;
+using Godot;
 using NedaoObjects;
 using System;
 using System.Buffers;
@@ -10,138 +10,138 @@ using System.Threading.Tasks;
 
 public partial class AttackProxy : Area2D
 {
-    private readonly HashSet<NedaoProxy> _nedaoProxies = [];
+	private readonly HashSet<NedaoProxy> _nedaoProxies = [];
 
-    private CollisionShape2D? _collisionShape;
-    private CircleShape2D? _shape;
+	private CollisionShape2D? _collisionShape;
+	private CircleShape2D? _shape;
 
-    [Export]
-    public NedaoProxy? Nedao
-    {
-        get; set;
-    }
+	[Export]
+	public NedaoProxy? Nedao
+	{
+		get; set;
+	}
 
-    [Export]
-    public CollisionShape2D? CollisionShape
-    {
-        get => _collisionShape;
-        set
-        {
-            if (_collisionShape is not null)
-            {
-                _collisionShape.Shape = null;
-            }
+	[Export]
+	public CollisionShape2D? CollisionShape
+	{
+		get => _collisionShape;
+		set
+		{
+			if (_collisionShape is not null)
+			{
+				_collisionShape.Shape = null;
+			}
 
-            _collisionShape = value;
+			_collisionShape = value;
 
-            if (_collisionShape is not null)
-            {
-                _collisionShape.Shape = CircleShape;
-            }
-        }
-    }
+			if (_collisionShape is not null)
+			{
+				_collisionShape.Shape = CircleShape;
+			}
+		}
+	}
 
-    public CircleShape2D CircleShape
-    {
-        get => _shape ??= new();
-    }
+	public CircleShape2D CircleShape
+	{
+		get => _shape ??= new();
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-        Monitoring = true;
+	public override void _Ready()
+	{
+		base._Ready();
+		Monitoring = true;
 
-        BodyEntered += OnBodyEntered;
-        BodyExited += OnBodyExited;
-    }
+		BodyEntered += OnBodyEntered;
+		BodyExited += OnBodyExited;
+	}
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-        if (Nedao is { } nedao)
-        {
-            CircleShape.Radius = nedao.Target.AttackRange;
+		if (Nedao is { } nedao)
+		{
+			CircleShape.Radius = nedao.Target.AttackRange;
 
-            if(nedao.CanAttack)
-            {
-                TryAttack(nedao);
-            }
-        }
-    }
+			if(nedao.CanAttack)
+			{
+				TryAttack(nedao);
+			}
+		}
+	}
 
-    protected virtual void TryAttack(NedaoProxy subject)
-    {
-        var temp = ArrayPool<NedaoProxy>.Shared.Rent(_nedaoProxies.Count);
-        _nedaoProxies.CopyTo(temp);
+	protected virtual void TryAttack(NedaoProxy subject)
+	{
+		var temp = ArrayPool<NedaoProxy>.Shared.Rent(_nedaoProxies.Count);
+		_nedaoProxies.CopyTo(temp);
 
-        try
-        {
-            for (var i = 0; i < _nedaoProxies.Count; i++)
-            {
-                var enemy = temp[i];
+		try
+		{
+			for (var i = 0; i < _nedaoProxies.Count; i++)
+			{
+				var enemy = temp[i];
 
 #if RELEASE
-                subject.TryAttack(enemy);
+				subject.TryAttack(enemy);
 #elif DEBUG
-                if (subject.TryAttack(enemy))
-                {
-                    GD.Print($"{subject.Name} attack {enemy.Name}; Enemy health: {enemy.Target.Health}")
-                }
+				if (subject.TryAttack(enemy))
+				{
+					GD.Print($"{subject.Name} attack {enemy.Name}; Enemy health: {enemy.Target.Health}");
+				}
 #endif
 
-            }
-        }
-        finally
-        {
-            ArrayPool<NedaoProxy>.Shared.Return(temp);
-        }
-        
-    }
+			}
+		}
+		finally
+		{
+			ArrayPool<NedaoProxy>.Shared.Return(temp);
+		}
+		
+	}
 
-    private void OnBodyEntered(Node2D body)
-    {
-        if (body is NedaoProxy nedao)
-        {
-            if(nedao == Nedao)
-            {
-                // Ignore self
-                return;
-            }
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is NedaoProxy nedao)
+		{
+			if(nedao == Nedao)
+			{
+				// Ignore self
+				return;
+			}
 
-            if(IsEnemy(nedao))
-            {
-                _nedaoProxies.Add(nedao);
+			if(IsEnemy(nedao))
+			{
+				_nedaoProxies.Add(nedao);
 
-                nedao.TreeExited += () => _nedaoProxies.Remove(nedao);
+				nedao.TreeExited += () => _nedaoProxies.Remove(nedao);
 
-                GD.Print("Attack");
-            }
-        }
-    }
+				GD.Print("Attack");
+			}
+		}
+	}
 
-    private void OnBodyExited(Node2D body)
-    {
-        if (body is NedaoProxy nedao)
-        {
-            if (nedao == Nedao)
-            {
-                // Ignore self
-                return;
-            }
+	private void OnBodyExited(Node2D body)
+	{
+		if (body is NedaoProxy nedao)
+		{
+			if (nedao == Nedao)
+			{
+				// Ignore self
+				return;
+			}
 
-            _nedaoProxies.Remove(nedao);
-        }
-    }
+			_nedaoProxies.Remove(nedao);
+		}
+	}
 
-    protected virtual bool IsEnemy(NedaoProxy subject)
-    {
-        return Nedao is not null && IsEnemy(Nedao, subject);
-    }
+	protected virtual bool IsEnemy(NedaoProxy subject)
+	{
+		return Nedao is not null && IsEnemy(Nedao, subject);
+	}
 
-    public static bool IsEnemy(NedaoProxy attacker, NedaoProxy defender)
-    {
-        return (attacker.Enemies & defender.Team) != 0;
-    }
+	public static bool IsEnemy(NedaoProxy attacker, NedaoProxy defender)
+	{
+		return (attacker.Enemies & defender.Team) != 0;
+	}
 
 }
